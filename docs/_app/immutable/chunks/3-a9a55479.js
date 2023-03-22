816337f1cc82685344bcb2cf31c0b62a5777f9f3
BaukeWestendorp <baukewestendorp@gmail.com>
2023-03-22 14:57:15 +0100

    Update /about page with more information

--- docs/_app/immutable/chunks/3-a9a55479.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-f2e901ed.js";export{t as component};
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,21 +11,49 @@
 
 <p>
 	Impossible Robotics is a non-profit organization located in the north of the Netherlands that's
-	composed of high school and college students. By participating in
-	<Link openInNewTab href="https://www.firstinspires.org/robotics/frc">FRC</Link>,
-	<Link openInNewTab href="https://first.global">FGC</Link> and
-	<Link openInNewTab href="https://www.firstinspires.org/robotics/ftc">FTC</Link>
-	our team aims to inspire and enthuse everyone from kids to teenagers and adults about technology. We
-	also try to forge a link between schools and employers.
+	composed of high school and college students. By participating in the
+	<Link openInNewTab href="https://www.firstinspires.org/robotics/frc">
+		First Robotics Competition
+	</Link>,
+	<Link openInNewTab href="https://first.global">First Global Challence</Link> and
+	<Link openInNewTab href="https://www.firstinspires.org/robotics/ftc">First Tech Challenge</Link>.
+	our team aims to inspire and enthuse everyone from kids to teenagers and adults about technology.
+	We also try to forge a link between schools and employers.
 </p>
-
 <img src="{base}/pages/about/team.jpg" alt="Our team" />
-
 <p>
 	<Link openInNewTab href="https://www.firstinspires.org/robotics/frc">FIRST</Link>
 	is an American organisation that took upon itself the task to promote technology worldwide. To achieve
 	this goal, FIRST, among other things, organises multiple competitions, including the FIRST Robotics
 	Competition.
+</p>
+<br />
+<p>
+	Our journey began with the <Link openInNewTab href="https://www.firstinspires.org/robotics/fll"
+		>First LEGO League</Link
+	>, after which we built our way up through FTC and eventually FRC. With a team growing bigger and
+	better each season, we take on the challenge to encourage the multidisciplinary commitment to
+	skills and technology.
+</p>
+<p>
+	Impossible Robotics was founded in august 2014, the year after we participated in FLL. <Link
+		openInNewTab
+		href="https://teamrembrandts.com"
+	>
+		Team Rembrandts
+	</Link>
+	taught us the essentials for having a FRC team. Now we are trying to progress our team each year and
+	teach our students the essentials of solving problems together and building robots, including engineering,
+	programming, time management, presentation skills, teamwork and leadership.
+</p>
+<br />
+<p>
+	Besides our work as an FRC team, we also attend lots of outreach events, where we are trying to
+	reach our goal of enthusing kids for technology. Besides all the above Impossible Robotics is a
+	fun, inclusive and intimate team. But the most important thing we want to effectuate is to create
+	an environment where the students can discover and expend their talents and interests, whether it
+	is technology related or social skills. We want to welcome all creativity and challenge the
+	impossible together.
 </p>
 
 <style>
